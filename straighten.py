c6e6083f59cc063eaa572fc56b5b0bd622962738
Daniel Elnatan <4480843+delnatan@users.noreply.github.com>
2023-03-30 12:07:18 -0700

    updated 'straighten' command to use residue selector

--- straighten.py
+++ straighten.py
@@ -11,59 +11,42 @@ to print to chimerax console, use `session.logger.info("string")
 
 
 def straighten(
-    session, model_id, start_residue, end_residue, chain_id="A", noise=3.0
+        session, residues, phi=-165.0, psi=165.0, omega=155.0, noise=3.0
 ):
     import numpy as np
-
-    session.logger.info(
-        "Straightening #%d: %d to %d" % (model_id, start_residue, end_residue)
-    )
-
-    model_id_str = "%d" % (model_id)
-
-    model = [m for m in session.models if m.id_string == model_id_str][0]
-    chain = [c for c in model.chains if c.chain_id == chain_id][0]
-    # missing residues are 'None', so we dont want them
-    residues = [
-        chain.residues[i]
-        for i in range(start_residue - 1, end_residue)
-        if chain.residues[i] is not None
-    ]
+    import random
+    
     nres = len(residues)
-    phi_arr = np.random.normal(loc=-140.0, scale=noise, size=nres)
-    psi_arr = np.random.normal(loc=130.0, scale=noise, size=nres)
+    
+    phi_arr = np.random.normal(loc=phi, scale=noise, size=nres)
+    psi_arr = np.random.normal(loc=psi, scale=noise, size=nres)
+    
     for i, res in enumerate(residues):
         if res.name == "PRO":
             # res.phi = np.random.normal(loc=-75.0, scale=noise, size=1)[0]
-            res.psi = np.random.normal(loc=150, scale=noise, size=1)[0]
-            res.omega = np.random.normal(loc=155, scale=noise, size=1)[0]
+            res.psi = random.gauss(psi, noise)
+            res.omega = random.gauss(omega, noise)
         else:
             res.phi = phi_arr[i]
             res.psi = psi_arr[i]
-        # session.logger.info(
-        #     "%s%d; phi=%.1f, psi=%.1f"
-        #     % (res.name, res.number, res.phi, res.psi)
-        # )
 
 
 def register_command(session):
     from chimerax.core.commands import (
         CmdDesc,
         register,
-        StringArg,
-        IntArg,
         FloatArg,
-        OpenFolderNameArg,
-        BoolArg,
     )
-
+    from chimerax.atomic import ResiduesArg
+    
     desc = CmdDesc(
         required=[
-            ("model_id", IntArg),
-            ("start_residue", IntArg),
-            ("end_residue", IntArg),
+            ("residues", ResiduesArg)
         ],
-        optional=[("chain_id", StringArg), ("noise", FloatArg)],
+        optional=[("noise", FloatArg),
+                  ("phi", FloatArg),
+                  ("psi", FloatArg),
+                  ("omega", FloatArg)],
         synopsis="straighten specified residues",
     )
 
