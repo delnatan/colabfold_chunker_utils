9a697691b9a2c4da137f1c0d2c013ab9e02da832
Daniel Elnatan <4480843+delnatan@users.noreply.github.com>
2024-12-19 22:07:13 -0800

    Re-implemented 'straighten.py' based on Eric and Elaine's feedback from ChimeraX

--- straighten.py
+++ straighten.py
@@ -1,64 +1,84 @@
-"""
-Custom ChimeraX commands
+def straighten(session, mobile_sel, mobile_terminal, noise=5.0):
+    """
+    Sets phi (ϕ) and psi (ψ) angles of specified residues
 
-See API:
-https://www.cgl.ucsf.edu/chimerax/docs/devel/py-modindex.html
+      ϕ     ψ     ω
+    N----Ca----C-----N
+         |
+         Cb
 
-guide:
-to print to chimerax console, use `session.logger.info("string")
+    Notes
+    -----
+    When selection is closer to C-terminus
+    N<->Ca: N terminal side is longer, smaller_side == "CA"
+    Ca<->C: C terminal side is shorter, smaller_side == "C"
 
-"""
+    When selection is closer to N-terminus
+    N<->Ca: N terminal side is shorter, smaller_side == "N"
+    Ca<->C: C terminal side is longer, smaller_side == "CA"
 
-
-def straighten(
-    session,
-    residues,
-    phi=-165.0,
-    psi=165.0,
-    omega=155.0,
-    noise=3.0,
-    move_smaller=True,
-):
-    import numpy as np
+    Args:
+    mobile_sel (selection): selection of residue regions to be moved, must be a
+    single contiguous region
+    mobile_terminal (str): 'N' or 'C' terminal side
+    noise (float): gaussian standard deviation to 'jitter' angles
+    """
     import random
 
-    nres = len(residues)
+    mobile_terminal = mobile_terminal.upper()
 
-    phi_arr = np.random.normal(loc=phi, scale=noise, size=nres)
-    psi_arr = np.random.normal(loc=psi, scale=noise, size=nres)
+    assert mobile_terminal.lower() in (
+        "c",
+        "n",
+    ), "mobile_terminal can only be N or C"
 
-    for i, res in enumerate(residues):
-        if res.name == "PRO":
-            # res.phi = np.random.normal(loc=-75.0, scale=noise, size=1)[0]
-            # res.psi = random.gauss(psi, noise)
-            # res.omega = random.gauss(omega, noise)
-            res.set_psi(
-                random.gauss(psi, noise), move_smaller_side=move_smaller
-            )
-            res.set_omega(
-                random.gauss(omega, noise), move_smaller_side=move_smaller
-            )
-        else:
-            # res.phi = phi_arr[i]
-            # res.psi = psi_arr[i]
-            res.set_phi(phi_arr[i], move_smaller_side=move_smaller)
-            res.set_psi(psi_arr[i], move_smaller_side=move_smaller)
+    for i, res in enumerate(mobile_sel):
+        # find backbone C-alpha
+        atom = res.find_atom("CA")
+
+        # handle proline differently (for now)
+        small_side_atoms = []
+
+        if res.name != "PRO":
+            # bond.smaller_side returns the atom that is on the smaller side
+            for nb, bond in zip(atom.neighbors, atom.bonds):
+                # look at peptide bond atoms only (no beta-carbons)
+                if nb.name == "N":
+                    small_side_atoms.append(bond.smaller_side.name)
+                elif nb.name == "C":
+                    small_side_atoms.append(bond.smaller_side.name)
+
+            # residue is near N-term
+            if mobile_terminal == "N":
+                move_smaller = set(small_side_atoms) == {"N", "CA"}
+            elif mobile_terminal == "C":
+                move_smaller = set(small_side_atoms) == {"CA", "C"}
+
+            psi_assgn = random.normalvariate(mu=135.0, sigma=noise)
+            res.set_psi(psi_assgn, move_smaller_side=move_smaller)
+
+            phi_assgn = random.normalvariate(mu=-139.0, sigma=noise)
+            res.set_phi(phi_assgn, move_smaller_side=move_smaller)
 
 
 def register_command(session):
-    from chimerax.core.commands import CmdDesc, register, FloatArg, BoolArg
     from chimerax.atomic import ResiduesArg
+    from chimerax.core.commands import (
+        CmdDesc,
+        FloatArg,
+        StringArg,
+        register,
+    )
 
     desc = CmdDesc(
-        required=[("residues", ResiduesArg)],
+        required=[("mobile_sel", ResiduesArg), ("mobile_terminal", StringArg)],
         optional=[
             ("noise", FloatArg),
-            ("phi", FloatArg),
-            ("psi", FloatArg),
-            ("omega", FloatArg),
-            ("move_smaller", BoolArg),
         ],
-        synopsis="straighten specified residues",
+        synopsis=(
+            "straighten specified residues. Choose N/C terminal"
+            " side to be moved"
+        ),
     )
 
     register("straighten", desc, straighten, logger=session.logger)
